812e4d9a0bd2b20d9d6835718d99601e91c43d08
Dirk Roorda <dirk@annotation.nl>
2020-03-22 13:26:21 +0100

    updates in pretty and plain

--- code/app.py
+++ code/app.py
@@ -1,8 +1,8 @@
-'''
-This module contains code for building the 
+"""
+This module contains code for building the
 North Eastern Neo Aramaic Text-Fabric app,
 which visualizes queries and results in TF.
-'''
+"""
 
 from tf.core.helpers import mdhtmlEsc, htmlEsc
 from tf.applib.helpers import dh as display_HTML
@@ -12,315 +12,320 @@ from tf.applib.api import setupApi
 from tf.applib.links import outLink
 from textwrap import dedent, indent
 
-plain_link = 'https://github.com/{org}/nena_corpus/blob/master/nena/{version}/{dialect}/{title}.nena'
+plain_link = (
+    "https://github.com/{org}/nena_corpus/blob/master/nena/"
+    "{version}/{dialect}/{title}.nena"
+)
 
-sections = {'dialect', 'title', 'line'}
-micros = {'letter', 'morpheme'}
-soft_border = {'inton'}
+sections = {"dialect", "title", "line"}
+micros = {"letter", "morpheme"}
+soft_border = {"inton"}
+
 
 class TfApp:
 
-    '''
+    """
     Constructs and delivers HTML for representing
     nodes in the Northeastern Neo-Aramaic TF corpus.
-    '''
-    
+    """
+
     def __init__(*args, **kwargs):
-        '''
+        """
         On init, sets up a standard TF api for
         interacting with the corpus.
-        '''
+        """
         setupApi(*args, **kwargs)
-        
+
     def webLink(app, n, text=None, className=None, _asString=False, _noUrl=False):
-        '''
-        Formats a HTML link to a source text 
+        """
+        Formats a HTML link to a source text
         that contains a supplied TF node.
-        '''
-        
+        """
+
         # make TF methods available
         api = app.api
         T = api.T
-        F = api.F
         version = app.version
-        
+
         # format string of node's embedding section
         dialect, text_title, line = T.sectionFromNode(n, fillup=True)
         passageText = app.sectionStrFromNode(n)
-                
+
         # format the link
         if not _noUrl:
-            href = plain_link.format(org=app.org, 
-                                     version=version,
-                                     dialect=dialect,
-                                     title=text_title) 
+            href = plain_link.format(
+                org=app.org, version=version, dialect=dialect, title=text_title
+            )
         # or return link to current page
         else:
-            href = '#'
-            
+            href = "#"
+
         # format the link text
-        if text is None:            
+        if text is None:
             text = passageText
-            title = 'see this passage in its source document'
+            title = "see this passage in its source document"
         else:
             title = passageText
 
         # returns a formatted anchor string
-        target = '' if _noUrl else None
-        link = outLink(text, 
-                       href, 
-                       title=title,
-                       className=className,
-                       target=target,
-                       passage=passageText)        
-        
+        target = "" if _noUrl else None
+        link = outLink(
+            text,
+            href,
+            title=title,
+            className=className,
+            target=target,
+            passage=passageText,
+        )
+
         # give the link
-        if _asString: 
+        if _asString:
             return link
-         # or show the link 
-        else: 
+        # or show the link
+        else:
             display_HTML(link)
-        
+
     def _plain(app, n, passage, isLinked, _asString, secLabel, **options):
-        '''
+        """
         Format a plain HTML representation of a TF node:
-        '''
-        
+        """
+
         # get display settings
         display = app.display
-        opts = display.get(options)
-        
+        d = display.get(options)
+
         # prepare api methods
-        _asApp = app._asApp # determine whether running in browser?
+        _asApp = app._asApp  # determine whether running in browser?
         api = app.api
-        L, T, F = api.L, api.T, api.F
-        
+        L, F = api.L, api.F
+
         # format and return HTML with format {section}{nodeUTF8}
         # the representation of the node depends on the node type and embedding
-        otype = F.otype.v(n)
+        nType = F.otype.v(n)
         result = passage
-        
+
         # configure HTML for node number rendering if requested
         if _asApp:
-            nodeRep = f' <a href="#" class="nd">{n}</a> ' if opts.withNodes else ''
+            nodeRep = f' <a href="#" class="nd">{n}</a> ' if d.withNodes else ""
         else:
-            nodeRep = f' <i>{n}</i> ' if opts.withNodes else ''
-            
+            nodeRep = f" <i>{n}</i> " if d.withNodes else ""
+
         # configure object's representation
-        isText = opts.fmt is None or '-orig-' in opts.fmt
-        
+        isText = d.fmt is None or "-orig-" in d.fmt
+
         # configure letter
-        if otype == 'letter':
-                        
+        if nType == "letter":
+
             # format text with any highlights
             # e.g. <span  class="hl"  style="background-color: green;">TEXT</span>
-            rep = hlText(app, [n], opts.highlights, fmt=opts.fmt)
-                        
+            rep = hlText(app, [n], d.highlights, fmt=d.fmt)
+
         # configure sections
-        elif otype in sections:
-            
-            if secLabel and opts.withPassage:
+        elif nType in sections:
+
+            if secLabel and d.withPassage:
                 rep = app.sectionStrFromNode(n)
             else:
-                rep = ''
-            
+                rep = ""
+
             # do not format sections as text segments
             isText = False
             rep = mdhtmlEsc(rep)
-            rep = hlRep(app, rep, n, opts.highlights)
-            
+            rep = hlRep(app, rep, n, d.highlights)
+
             # configure lines to show words as well
-            if otype == 'line':
-                
+            if nType == "line":
+
                 # first add link if necessary
                 if isLinked:
-                    rep = app.webLink(n, text=rep, className='ln', _asString=True)
+                    rep = app.webLink(n, text=rep, className="ln", _asString=True)
                 else:
                     rep = f'<span class="ln">{rep}</span>'
-                
+
                 # then add words from the line
-                rep += hlText(app, L.d(n, otype='letter'), opts.highlights, fmt=opts.fmt)
-                isText = True # treat line like text
-                
+                rep += hlText(app, L.d(n, otype="letter"), d.highlights, fmt=d.fmt)
+                isText = True  # treat line like text
+
         # configure all other otypes
         else:
-            rep = hlText(app, L.d(n, otype='word'), opts.highlights, fmt=opts.fmt)
-            
+            rep = hlText(app, L.d(n, otype="word"), d.highlights, fmt=d.fmt)
+
         # configure links
 
-        if isLinked and otype != 'line':
+        if isLinked and not passage and nType != "line":
             rep = app.webLink(n, text=rep, _asString=True)
-                
+
         # finalize span and add formatted string
-        
-        tClass = display.formatClass[opts.fmt] if isText else 'trb' # div class   
+
+        tClass = display.formatClass[d.fmt] if isText else "trb"  # div class
         rep = f'<span class="{tClass}">{rep}</span>'
-        result += f'{rep}{nodeRep}'
-        
+        result += f"{rep}{nodeRep}"
+
         # return as string
         if _asString or _asApp:
             return result
-        
+
         # or display
         else:
             display_HTML(result)
-            
+
     def _pretty(app, n, outer, html, firstSlot, lastSlot, **options):
-        '''
+        """
         Formats a TF node with pretty HTML formatting.
-        '''
+        """
 
         # get display settings
         display = app.display
-        opts = display.get(options)
-        
+        d = display.get(options)
+
         # preprocess and validate node
-        pre = prettyPre(app, n, firstSlot, lastSlot, opts.withNodes, opts.highlights)
-        
+        goOn = prettyPre(app, n, firstSlot, lastSlot, d)
+
         # error out
-        if not pre:
+        if not goOn:
             return
-        
+
         # unpackage preprocessed data
-        slotType = pre[0] # slot type in databe
-        otype = pre[1] # node's object type in database
-        className = pre[2] # default div class for this otype
-        boundaryClass = pre[3] # ?div class for boundary?
-        hlAtt = pre[4] # div class for highlighted nodes
-        nodePart = pre[5] # html repre. of node number
-        myStart = pre[6] # first slot number in node
-        myEnd = pre[7] # last slot number in node
-        
+        # slotType : slot type in database
+        # nType : node's object type in database
+        # className : default div class for this node type
+        # boundaryClass : ?div class for boundary?
+        # hlAtt : div class for highlighted nodes
+        # nodePart : html repre. of node number
+        # myStart : first slot number in node
+        # myEnd : last slot number in node
+
+        goOn = prettyPre(app, n, firstSlot, lastSlot, d)
+        if not goOn:
+            return
+        (
+            slotType,
+            nType,
+            isBigType,
+            className,
+            boundaryClass,
+            hlAtt,
+            nodePart,
+            myStart,
+            myEnd,
+        ) = goOn
+
         # prepare TF api methods and data
         api = app.api
-        L, F, T = api.L, api.F, api.T
-        otypeRank = api.otypeRank
-        isHtml = options.get('fmt', None) in app.textFormats        
-        
-        # determine size of object
-        # objects bigger than condense type will not have
-        # any children
-        bigType = False
-        condense = opts.condenseType
-        if condense and otypeRank[otype] > otypeRank[condense]:
-            bigType = True
-        
+        F = api.F
+        L = api.L
+        T = api.T
+        isHtml = options.get("fmt", None) in app.textFormats
+
         # determine whether object is outermost object
         # if it is and it is also a micro, toggle showMicro to True
         # this determines whether letter/morpheme gets borders and features
         if outer:
             html.append('<div class="outeritem">')
-            if otype in micros:
-                opts.showMicro = True
-        
+            if nType in micros:
+                d.showMicro = True
+
         # skip non-displayed micros
-        if otype in micros and not opts.showMicro:
+        if nType in micros and not d.showMicro:
             return
-            
+
         # determine embedded objects to show
         # these will be called recursively
-        if bigType:
+        if isBigType:
             children = ()
-        elif otype == 'letter':
+        elif nType == "letter":
             children = ()
-        elif otype == 'morpheme':
-            children = L.d(n, 'letter')
-        elif otype == 'word':
-            children = L.d(n, 'morpheme')
-        elif otype == 'inton':
-            children = L.d(n, 'word')
-        elif otype == 'sentence':
-            children = L.d(n, 'inton')
-        elif otype == 'line':
-            children = L.d(n, 'sentence')
+        elif nType == "morpheme":
+            children = L.d(n, "letter")
+        elif nType == "word":
+            children = L.d(n, "morpheme")
+        elif nType == "inton":
+            children = L.d(n, "word")
+        elif nType == "sentence":
+            children = L.d(n, "inton")
+        elif nType == "line":
+            children = L.d(n, "sentence")
         else:
-            children = L.d(n, otype='word')
-        
+            children = L.d(n, otype="word")
+
         # --
         # OPEN the div for the node
         # set the border attribute and other classes accordingly
         # --
-                                
-        hlClass, hlStyle = hlAtt # highlighting attributes
-        
+
+        hlClass, hlStyle = hlAtt  # highlighting attributes
+
         # package it all up:
         html.append(f'<div class="{className} {boundaryClass} {hlClass}" {hlStyle}>')
-        
+
         # format section text to appear over all items
-        if otype in sections:
+        doShowFeatures = d.showFeatures or (d.showFeatures is None and d.extraFeatures)
+
+        if nType in sections:
             passage = app.webLink(n, _asString=True)
             featurePart = getFeatures(app, n, (), **options)
-            
-            sectionHTML = f'''
+
+            sectionHTML = f"""
             <div class="ll">
                 <div class="line">{passage}</div>
                 {nodePart}
                 {featurePart}
             </div>
-            '''
-            sectionHTML = indent(dedent(sectionHTML), '    ')
+            """
+            sectionHTML = indent(dedent(sectionHTML), "    ")
             html.append(sectionHTML)
-            
+
         # format micro objects
-        elif otype in micros and opts.showMicro:
-            
-            if otype == 'letter':
-                if opts.fmt == 'text-trans-full':
+        elif nType in micros and d.showMicro:
+
+            if nType == "letter":
+                if d.fmt == "text-trans-full":
                     text = F.trans_f.v(n)
-                elif opts.fmt == 'text-trans-lite':
+                elif d.fmt == "text-trans-lite":
                     text = F.trans_l.v(n)
                 else:
                     text = F.text.v(n)
-                text = htmlEsc(text)
+                text = text if isHtml else htmlEsc(text)
                 textHTML = f'<div class="ara">{text}</div>'
                 html.append(textHTML)
-            
+
             # show additional features only if asked
-            if opts.showFeatures:
-                featurePart = getFeatures(app, n, (), **options)
-            else:
-                featurePart = ''
-            nodePart = nodePart or ''
-            nodeHTML = f'{nodePart}{featurePart}'
+            featurePart = getFeatures(app, n, (), **options) if doShowFeatures else ""
+            nodePart = nodePart or ""
+            nodeHTML = f"{nodePart}{featurePart}"
             html.append(nodeHTML)
 
-        elif otype == 'word' and not opts.showMicro:
-            text = T.text([n], fmt=opts.fmt)
-            text = htmlEsc(text)
+        elif nType == "word" and not d.showMicro:
+            text = T.text([n], fmt=d.fmt)
+            text = text if isHtml else htmlEsc(text)
             textHTML = f'<div class="ara">{text}</div>'
             html.append(textHTML)
-            
+
             # do features of word
-            nodePart = nodePart or ''
-            if opts.showFeatures:
-                featurePart = getFeatures(app, n, (), **options)
-            else:
-                featurePart = ''
-            html.append(f'{nodePart}{featurePart}')
-            
+            nodePart = nodePart or ""
+            featurePart = getFeatures(app, n, (), **options) if doShowFeatures else ""
+            html.append(f"{nodePart}{featurePart}")
+
         # format everything else
         else:
-            
+
             # add node number if asked
-            nodePart = nodePart or ''
-            if opts.showFeatures:
-                featurePart = getFeatures(app, n, (), **options)
-            else:
-                featurePart = ''
-            html.append(f'{nodePart}{featurePart}')
-                
+            nodePart = nodePart or ""
+            featurePart = getFeatures(app, n, (), **options) if doShowFeatures else ""
+            html.append(f"{nodePart}{featurePart}")
+
             # for now, do nothing more
             # ...
-        
+
         # format children with recursive call
         for child in children:
             app._pretty(child, False, html, firstSlot, lastSlot, **options)
-        
+
         # --
         # CLOSE the node's div
         # --
-        html.append('</div>')
-        
+        html.append("</div>")
+
         # close outer div if necessary
         if outer:
-            html.append('</div>')
+            html.append("</div>")
--- code/config.py
+++ code/config.py
@@ -1,79 +1,76 @@
 from os.path import dirname, abspath
 
-PROTOCOL = 'http://'
-HOST = 'localhost'
-PORT = {'kernel':19700,
-        'web':9700}
+PROTOCOL = "http://"
+HOST = "localhost"
+PORT = {"kernel": 19700, "web": 9700}
 
 OPTIONS = (
-    ('showMicro', False, 'checkbox', 'subwordc', 'show char and morpheme boundaries'),
-     ('showFeatures', False, 'checkbox', '', 'show feature values'), 
-          )
+    ("showMicro", False, "checkbox", "subwordc", "show char and morpheme boundaries"),
+    ("showFeatures", None, "checkbox", "", "show feature values"),
+)
 
-ORG = 'CambridgeSemiticsLab'
-REPO = 'nena_tf'
-CORPUS = 'Northeastern Neo-Aramaic Text Corpus'
-VERSION = '0.01'
-RELATIVE = 'tf'
+ORG = "CambridgeSemiticsLab"
+REPO = "nena_tf"
+CORPUS = "Northeastern Neo-Aramaic Text Corpus"
+VERSION = "0.01"
+RELATIVE = "tf"
 
-DOI_TEXT = '10.5281/zenodo.3250721'
-DOI_URL = 'https://doi.org/10.5281/zenodo.3250721'
+DOI_TEXT = "10.5281/zenodo.3250721"
+DOI_URL = "https://doi.org/10.5281/zenodo.3250721"
 
-DOC_URL = f'https://github.com/{ORG}/{REPO}/blob/master/docs'
-DOC_INTRO = 'features.md'
-CHAR_URL = f'https://github.com/{ORG}/{REPO}/blob/master/docs/transcription.md'
-CHAR_TEXT = 'NENA transcription script',
+DOC_URL = f"https://github.com/{ORG}/{REPO}/blob/master/docs"
+DOC_INTRO = "features.md"
+CHAR_URL = f"https://github.com/{ORG}/{REPO}/blob/master/docs/transcription.md"
+CHAR_TEXT = ("NENA transcription script",)
 
-FEATURE_URL = f'{DOC_URL}/features.md#{{feature}}'
+FEATURE_URL = f"{DOC_URL}/features.md#{{feature}}"
 
 MODULE_SPECS = ()
 
 ZIP = [REPO]
 
-CONDENSE_TYPE = 'sentence'
+CONDENSE_TYPE = "sentence"
 
-NONE_VALUES = {None, 'NA', 'none', 'unknown'} # TO REVISIT 
+NONE_VALUES = {None, "NA", "none", "unknown"}  # TO REVISIT
 
-STANDARD_FEATURES = '''
-
+STANDARD_FEATURES = """
     dialect title text trailer
-    
-'''.strip().split()
+""".strip().split()
 
 EXCLUDED_FEATURES = set()
 
 NO_DESCEND_TYPES = {}
 
 EXAMPLE_SECTION = (
-    f'<code>Village Life 1.1</code> (use'
+    f"<code>Village Life 1.1</code> (use"
     f' <a href="https://github.com/{ORG}/{REPO}'
     f'/blob/master/tf/{VERSION}/book%40en.tf" target="_blank">'
-    f'English book names</a>)'
+    f"English book names</a>)"
 )
-EXAMPLE_SECTION_TEXT = 'Barwar, The Monk and the Angel, 1'
+EXAMPLE_SECTION_TEXT = "Barwar, The Monk and the Angel, 1"
 
-SECTION_SEP1 = ', '
-SECTION_SEP2 = ', Ln. '
+SECTION_SEP1 = ", "
+SECTION_SEP2 = ", Ln. "
 
-DEFAULT_CLS = 'trb'
-DEFAULT_CLS_ORIG = 'ara'
+DEFAULT_CLS = "trb"
+DEFAULT_CLS_ORIG = "ara"
 
-FORMAT_CSS = {'orig':DEFAULT_CLS_ORIG,
-              'trans':DEFAULT_CLS
-             }
+FORMAT_CSS = {"orig": DEFAULT_CLS_ORIG, "trans": DEFAULT_CLS}
 
-CLASS_NAMES = {'letter':'micro',
-               'morpheme':'micro',
-               'word':'word',
-               'subsentence':'macro',
-               'inton':'prosa',
-               'sentence':'macro',
-               'line':'line'}
+CLASS_NAMES = {
+    "letter": "micro",
+    "morpheme": "micro",
+    "word": "word",
+    "subsentence": "macro",
+    "inton": "prosa",
+    "sentence": "macro",
+    "line": "line",
+}
 
-# fonts are from 
-FONT_NAME = 'CharisSIL-R'
-FONT = 'CharisSIL-R.otf'
-FONTW = 'CharisSIL-R.woff'
+# fonts are from
+FONT_NAME = "CharisSIL-R"
+FONT = "CharisSIL-R.otf"
+FONTW = "CharisSIL-R.woff"
 
 TEXT_FORMATS = {}
 
